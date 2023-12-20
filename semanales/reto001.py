76202ed83153e8557767346b1c1f92ecd78cf06f
zeristdaw <zeristdaw@gmail.com>
2023-12-20 19:27:11 +0100

    reto 1 realizado

--- semanales/reto001.py
+++ semanales/reto001.py
@@ -10,3 +10,15 @@
  */
 
 '''
+
+diccionario = {"a" : "4", "b" : "l3", "c" : "[", "d" : ")", "e" : "3", "f" : "|=", "g" : "&", "h" : "#", "i" : "1",
+               "j" : ",_|", "k" : ">|", "l" : "1", "m" : "jvi", "n" : "|\|", "o" : "0", "p" : "\*", "q" : "(_,)", "r" : "I2",
+               "s" : "5", "t" : "7", "u" : "(_)", "v" : "\/", "w" : "VV", "x" : "><", "y" : "j", "z" : "2"}
+
+palabra_entrada = input("Escribe una palabra: ")
+
+palabra_limpia = palabra_entrada.casefold()
+palabra_traducida = ""
+for i in palabra_limpia:
+    palabra_traducida = palabra_traducida + diccionario[i]
+print(palabra_traducida)
