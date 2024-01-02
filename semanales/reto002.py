60339d70943fc237fc73c7f1c691311413726ac4
zerist <zeristdaw@gmail.com>
2024-01-02 12:44:56 +0100

    reto002 realizado

--- semanales/reto002.py
+++ semanales/reto002.py
@@ -19,3 +19,55 @@
  */
 '''
 
+jugador1 = ""
+jugador2 = ""
+
+contador1 = 0
+contador2 = 0
+
+secuencia = ["p1" ,"p1" ,"p2" ,"p2" ,"p1" ,"p2" ,"p1" ,"p1"]
+
+for i in secuencia:
+
+    if i == "p1":
+        contador1 = contador1 + 1
+    if i == "p2":
+        contador2 = contador2 + 1
+
+    if contador1 == 0:
+        jugador1 = "love"
+    if contador1 == 1:
+        jugador1 = "15"
+    if contador1 == 2:
+        jugador1 = "30"
+
+    if contador2 == 0:
+        jugador2 = "love"
+    if contador2 == 1:
+        jugador2 = "15"
+    if contador2 == 2:
+        jugador2 = "30"
+
+    if contador1 >= 3 and contador1 == contador2:
+        jugador1 = "Deuce"
+        jugador2 = ""
+    if contador1 == 3 and contador2 < 3:
+        jugador1 = "40"
+    if contador2 == 3 and contador1 < 3:
+        jugador2 = "40"
+
+    if contador1 > 3 and contador1 == contador2 + 1:
+        jugador1 = "Ventaja jugador1"
+    if contador1 > 3 and contador1 == contador2 + 2:
+        jugador1 = "Victoria jugador1"
+
+    if contador2 > 3 and contador2 == contador1 + 1:
+        jugador1 = "Ventaja jugador2"
+    if contador2 > 3 and contador2 == contador1 + 2:
+        jugador1 = "Victoria jugador2"
+
+    if jugador1 == "Deuce" or jugador1 == "Ventaja jugador1" or jugador1 == "Victoria jugador1" or jugador1 == "Ventaja jugador2" or jugador1 == "Victoria jugador2":
+        print(f'{jugador1}')
+    else:
+        print(f'{jugador1} - {jugador2}')
+
